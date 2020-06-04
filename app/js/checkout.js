132fa7040bdfdad05d3029c2ed8672129c0e2ef1
dmho18 <dmho@ucsd.edu>
2020-06-04 04:12:08 -0700

    fix checkout

--- app/js/checkout.js
+++ app/js/checkout.js
@@ -1,5 +1,7 @@
 
-const { checkTeams, endFlow, cancel } = require('./js_functions/checkout_functions.js');
+const {
+  checkTeams, endFlow, cancel, setColor,
+} = require('./js_functions/checkout_functions.js');
 
 /** Firebase Config */
 const firebaseConfig = {
@@ -25,3 +27,35 @@ endFlowButton.addEventListener('click', () => endFlow(db, uid));
 
 const cancelButton = document.getElementById('cancelBtn');
 cancelButton.addEventListener('click', () => cancel());
+
+// const completedBtn1 = document.getElementById('completedBtn1');
+// completedBtn1.addEventListener('click', () => setColor('completedBtn', '#7FFF00', 1));
+
+// const completedBtn2 = document.getElementById('completedBtn2');
+// completedBtn2.addEventListener('click', () => setColor('completedBtn', '#7FFF00', 2));
+
+// const completedBtn1 = document.getElementById('completedBtn3');
+// completedBtn3.addEventListener('click', () => setColor('completedBtn', '#7FFF00', 3));
+
+// const keepBtn1 = document.getElementById('keepBtn1');
+// keepBtn1.addEventListener('click', () => setColor('keepBtn', '#00B4AB', 1));
+// const keepBtn2 = document.getElementById('keepBtn2');
+// keepBtn2.addEventListener('click', () => setColor('keepBtn', '#00B4AB',2));
+// const keepBtn3 = document.getElementById('keepBtn3');
+// keepBtn3.addEventListener('click', () => setColor('keepBtn', '#00B4AB',3));
+
+// const sosBtn1 = document.getElementById('sosBtn1');
+// sosBtn1.addEventListener('click', () => setColor('sosBtn', '#00B4AB',1));
+// const sosBtn2 = document.getElementById('sosBtn2');
+// sosBtn2.addEventListener('click', () => setColor('sosBtn', '#00B4AB',2));
+// const sosBtn3 = document.getElementById('sosBtn3');
+// sosBtn3.addEventListener('click', () => setColor('sosBtn', '#00B4AB',3));
+
+// const blockedBtn1 = document.getElementById('blockedBtn1');
+// blockedBtn1.addEventListener('click', () => setColor('blockedBtn', 'red',1));
+
+// const blockedBtn2 = document.getElementById('blockedBtn2');
+// blockedBtn2.addEventListener('click', () => setColor('blockedBtn', 'red',2));
+
+// const blockedBtn3 = document.getElementById('blockedBtn3');
+// blockedBtn3.addEventListener('click', () => setColor('blockedBtn', 'red',3));
--- app/js/js_functions/checkout_functions.js
+++ app/js/js_functions/checkout_functions.js
@@ -1,7 +1,136 @@
+/* eslint no-shadow: ["error", { "allow": ["teamName"] }] */
+
+let teamName = '';
 const { dialog } = require('electron').remote;
 
 const errorMessage = 'An error occurred when trying to find your team, returning to main page.';
 
+// a dictionary to check if any button is clicked
+const dict = {};
+const c = 'completedBtn';
+const k = 'keepBtn';
+const s = 'sosBtn';
+const b = 'blockedBtn';
+let j;
+for (j = 1; j <= 3; j += 1) {
+  dict[j] = {};
+  dict[j][c] = 1;
+  dict[j][k] = 1;
+  dict[j][s] = 1;
+  dict[j][b] = 1;
+}
+const colors = {};
+colors[c] = '#7FFF00';
+colors[k] = '#00B4AB';
+colors[s] = '#FFD832';
+colors[b] = 'red';
+
+/**
+ * set the color of a row of button to the desired
+ * @param {*} btn
+ * @param {*} color
+ * @param {*} i
+ */
+function setColor(btn, color, i) {
+  const keys = Object.keys(dict[i]);
+  for (let n = 0; n < keys.length; n += 1) {
+    const property = document.getElementById(keys[n] + i.toString());
+    if (keys[n] === btn) {
+      if (dict[i][btn] === 0) {
+        property.style.backgroundColor = '#FFFFFF';
+        dict[i][btn] = 1;
+      } else {
+        property.style.backgroundColor = color;
+        dict[i][btn] = 0;
+      }
+    } else {
+      property.style.backgroundColor = '#FFFFFF';
+      dict[i][keys[n]] = 1;
+    }
+  }
+}
+for (j = 1; j <= 3; j += 1) {
+  setColor(c, '#7FFF00', j);
+}
+
+/**
+ * create a list of listeners for buttons according to row i
+ * @param {*} btn
+ * @param {*} i
+ */
+function setListener(i) {
+  const keys = Object.keys(dict[i]);
+  for (let n = 0; n < keys.length; n += 1) {
+    const property = document.getElementById(keys[n] + i.toString());
+    property.addEventListener('click', () => setColor(keys[n], colors[keys[n]], i));
+  }
+}
+
+/**
+ * create a list of goals that user saved in check-in
+ * @param {*} goal
+ * @param {*} n
+ */
+function createGoalList(goal, n) {
+  // Assigning the attributes
+  const id = n.toString();
+  const form = document.getElementById(`line${id}`);
+  const label = document.createElement('label');
+  const labelId = `task${id}`;
+  const con = document.getElementById(`container${id}`);
+
+
+  // appending the created text to
+  // the created label tag
+  const str = '';
+  label.appendChild(document.createTextNode(goal + str));
+  label.id = labelId;
+
+
+  document.getElementById(`h${id}`).style.display = 'block';
+
+  con.style.position = 'absolute';
+  con.style.right = '0';
+  con.style.display = 'inline-block';
+
+  // appending label to div
+  form.appendChild(label);
+  form.appendChild(con);
+}
+
+/**
+ * TODO
+ */
+function updateGoal(db, uid) {
+  let n = 1;
+  const goalText = document.getElementById('goalText');
+  const docRef = db.collection('teams').doc(teamName).collection(uid).doc('status');
+  docRef.get()
+    .then((doc) => {
+      if (doc.exists) {
+        goalText.style.display = 'none';
+        let id = `task${n.toString()}`;
+        const data = doc.data();
+        while (id in data && data[id] !== '') {
+          createGoalList(data[id], n);
+          setListener(n);
+          n += 1;
+          // taskNum++;
+          id = `task${n.toString()}`;
+        }
+        if (n === 1) {
+          goalText.innerHTML = 'No Task Set For The Day!';
+          goalText.style.display = 'block';
+        }
+      } else {
+        console.error('Error getting data');
+      }
+    })
+    .catch((error) => {
+      console.error('Error getting data: ', error);
+      // document.location.href = 'taskbar.html'
+    });
+}
 
 /**
  * Checks which team a particular user is currently in
@@ -35,82 +164,6 @@ function checkTeams(uid, db) {
 }
 
 /**
- * create a list of goals that user saved in check-in
- * @param {*} goal
- * @param {*} n
- */
-function createGoalList(goal, n) {
-  // Assigning the attributes
-  const id = n.toString();
-  const form = document.getElementById(`line${id}`);
-  const label = document.createElement('label');
-  const labelId = `task${id}`;
-  const con = document.getElementById(`container${id}`);
-
-
-  // appending the created text to
-  // the created label tag
-  const s = '';
-  label.appendChild(document.createTextNode(goal + s));
-  label.id = labelId;
-
-
-  document.getElementById(`h${id}`).style.display = 'block';
-
-  con.style.position = 'absolute';
-  con.style.right = '0';
-  con.style.display = 'inline-block';
-
-  // appending label to div
-  form.appendChild(label);
-  form.appendChild(con);
-}
-
-let taskNum = 1;
-
-/**
- * TODO
- */
-function updateGoal(db, uid) {
-  let n = 1;
-  const goalText = document.getElementById('goalText');
-  const docRef = db.collection('teams').doc(teamName).collection(uid).doc('status');
-  docRef.get()
-    .then((doc) => {
-      if (doc.exists) {
-        goalText.style.display = 'none';
-        let id = `task${n.toString()}`;
-        const data = doc.data();
-        while (id in data & data[id] != '') {
-          createGoalList(data[id], n);
-          n++;
-          taskNum++;
-          id = `task${n.toString()}`;
-        }
-        if (n == 1) {
-          goalText.innerHTML = 'No Task Set For The Day!';
-          goalText.style.display = 'block';
-        }
-      } else {
-        console.error('Error getting data');
-      }
-    })
-    .catch((error) => {
-      console.error('Error getting data: ', error);
-      // document.location.href = 'taskbar.html'
-    });
-}
-
-/**
- * Starts endflow
- * @param {*} db
- * @param {string} uid
- */
-function endFlow(db, uid) {
-  updateThermometer(db, uid);
-}
-
-/**
  * After thermometer is updated Firebase db is updated with the completed statuses
  */
 function handleEndFlow(db, uid) {
@@ -119,21 +172,22 @@ function handleEndFlow(db, uid) {
   const obj = {
     checkedIn: false,
   };
-  for (i = 1; i < 4; i++) {
+  let i;
+  for (i = 1; i < 4; i += 1) {
     const id = i.toString();
     const taskId = `task${id}`;
     const taskStatus = `taskStatus${id}`;
     const element = document.getElementById(taskId);
-    if (element != null) {
+    if (element !== null) {
       const t = element.textContent;
 
       // obj[taskId] = t;
       obj[taskStatus] = 0;
-      if (dict[i][k] == 0) obj[taskStatus] = 1;
-      else if (dict[i][s] == 0) obj[taskStatus] = 2;
-      else if (dict[i][b] == 0) obj[taskStatus] = 3;
+      if (dict[i][k] === 0) obj[taskStatus] = 1;
+      else if (dict[i][s] === 0) obj[taskStatus] = 2;
+      else if (dict[i][b] === 0) obj[taskStatus] = 3;
 
-      if (obj[taskStatus] == 0) obj[taskId] = '';
+      if (obj[taskStatus] === 0) obj[taskId] = '';
       else obj[taskId] = t;
     } else {
       obj[taskId] = '';
@@ -171,15 +225,15 @@ function updateThermometer(db, uid) {
   if (window.getComputedStyle(line3).display === 'block') line3Valid = true;
   let tasksCompleted = 0;
   if (line1Valid) {
-    if (dict[1].completedBtn == 0) tasksCompleted++;
+    if (dict[1].completedBtn === 0) tasksCompleted += 1;
     else console.log('Task 1 not completed');
   }
   if (line2Valid) {
-    if (dict[2].completedBtn == 0) tasksCompleted++;
+    if (dict[2].completedBtn === 0) tasksCompleted += 1;
     else console.log('Task 2 not completed');
   }
   if (line3Valid) {
-    if (dict[3].completedBtn == 0) tasksCompleted++;
+    if (dict[3].completedBtn === 0) tasksCompleted += 1;
   }
   console.log(tasksCompleted);
   console.log(teamName);
@@ -228,11 +282,22 @@ function updateThermometer(db, uid) {
     });
 }
 
+
+/**
+ * Starts endflow
+ * @param {*} db
+ * @param {string} uid
+ */
+function endFlow(db, uid) {
+  updateThermometer(db, uid);
+}
+
+
 /**
  * Cancels checkout flow
  */
 function cancel() { document.location.href = 'taskbar.html'; }
 
 module.exports = {
-  checkTeams, createGoalList, updateGoal, endFlow, updateThermometer, cancel,
+  checkTeams, createGoalList, updateGoal, endFlow, updateThermometer, cancel, setColor,
 };
