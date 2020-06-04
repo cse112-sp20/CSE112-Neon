5d15b18dc0d2f519035e0122574b66c5d1ae88ce
dmho18 <dmho@ucsd.edu>
2020-06-04 01:42:31 -0700

    fix googlesignin

--- app/js/googlesignin.js
+++ app/js/googlesignin.js
@@ -20,13 +20,13 @@ firebase.initializeApp(firebaseConfig);
 const h3 = document.getElementById('loginstatus');
 
 /**
- * Gets redirect result, if there isn't a redirect result then redirects page to Google sign in page. After
- * redirect result is obtained then post to server.
+ * Gets redirect result, if there isn't a redirect result then redirects page
+ * to Google sign in page. After redirect result is obtained then post to server.
  */
 firebase.auth().getRedirectResult().then((result) => {
   if (result.credential) {
     // This gives you a Google Access Token. You can use it to access the Google API.
-    const token = result.credential.accessToken;
+    // const token = result.credential.accessToken;
     // ...
   }
   // The signed-in user info.
@@ -50,19 +50,20 @@ firebase.auth().getRedirectResult().then((result) => {
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function () { // Call a function when the state changes.
       if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        const response = JSON.parse(xhr.response);
+        // const response = JSON.parse(xhr.response);
       }
     };
     xhr.send(JSON.stringify(id));
     h3.innerHTML = 'You have successfully logged in, please return to Neon Pulse.';
   }
 }).catch((error) => {
+  console.log(error);
   // Handle Errors here.
-  const errorCode = error.code;
-  const errorMessage = error.message;
+  // const errorCode = error.code;
+  // const errorMessage = error.message;
   // The email of the user's account used.
-  const { email } = error;
+  // const { email } = error;
   // The firebase.auth.AuthCredential type that was used.
-  const { credential } = error;
+  // const { credential } = error;
   // ...
 });
