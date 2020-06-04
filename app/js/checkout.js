8a059646ad36afe7814f499ebea41552d0581761
Justin <jggross@ucsd.edu>
2020-06-04 16:20:33 -0700

    Run autolint again and fix another lint error in checkout.js

--- app/js/checkout.js
+++ app/js/checkout.js
@@ -1,18 +1,18 @@
 // const firebase = require('firebase');
 const {
-    checkTeams, endFlow, cancel,
+  checkTeams, endFlow, cancel,
 } = require('./js_functions/checkout_functions.js');
 
 /** Firebase Config */
 const firebaseConfig = {
-    apiKey: 'AIzaSyBmn_tDSlm4lLdrvSqj8Yb00KkYae8cL-Y',
-    authDomain: 'neon-pulse-development.firebaseapp.com',
-    databaseURL: 'https://neon-pulse-development.firebaseio.com',
-    projectId: 'neon-pulse-development',
-    storageBucket: 'neon-pulse-development.appspot.com',
-    messagingSenderId: '240091062123',
-    appId: '1:240091062123:web:babe11f5f03ced38fbb62e',
-    measurementId: 'G-VMS6JL8H4S',
+  apiKey: 'AIzaSyBmn_tDSlm4lLdrvSqj8Yb00KkYae8cL-Y',
+  authDomain: 'neon-pulse-development.firebaseapp.com',
+  databaseURL: 'https://neon-pulse-development.firebaseio.com',
+  projectId: 'neon-pulse-development',
+  storageBucket: 'neon-pulse-development.appspot.com',
+  messagingSenderId: '240091062123',
+  appId: '1:240091062123:web:babe11f5f03ced38fbb62e',
+  measurementId: 'G-VMS6JL8H4S',
 };
 
 /** Initialize Firebase */
@@ -21,8 +21,6 @@ const db = firebase.firestore();
 const uid = localStorage.getItem('userid');
 
 checkTeams(uid, db);
-
-document.addEventListener('load', () => initButtons());
 
 const endFlowButton = document.getElementById('endFlowBtn');
 endFlowButton.addEventListener('click', () => endFlow(db, uid));
