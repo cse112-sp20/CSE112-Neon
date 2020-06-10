3517e06cab9dc9aae184c9555290e9f2448e4090
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Pro-2.local>
2020-06-09 19:31:53 -0700

    Adding thermometer fix

--- app/js/createteam.js
+++ app/js/createteam.js
@@ -13,29 +13,34 @@ const joinTeamButton = document.getElementById('createBtn');
  * Creates a new team and creates a new thermometer for a given teamname
  */
 joinTeamButton.addEventListener('click', () => {
-  console.log('Clicked');
-  const teamName = document.getElementById('teamName').value;
-  console.log(teamName);
-  console.log(teamName);
-  db.collection('users').doc(uid).update({ team: teamName });
-  const obj = {};
-  obj[uid] = true;
-  db.collection('teams').doc(teamName).set(obj)
-    .then(() => {
-      console.log('Document written');
-      const objNew = {};
-      objNew.progress = 0;
-      db.collection('thermometers').doc(teamName).set(objNew).then(() => {
-        console.log(`Created new thermometer for ${teamName}`);
-        document.location.href = 'taskbar.html';
-      })
+    console.log('Clicked');
+    const teamName = document.getElementById('teamName').value;
+    console.log(teamName);
+    console.log(teamName);
+    db.collection('users').doc(uid).update({ team: teamName });
+    const obj = {};
+    obj[uid] = true;
+    db.collection('teams').doc(teamName).set(obj)
+        .then(() => {
+            console.log('Document written');
+            const objNew = {};
+            const newDay = new Date();
+            newDay.setHours(0);
+            newDay.setMinutes(0);
+            newDay.setSeconds(0);
+            objNew.progress = 0;
+            objNew.lastEpoch = newDay.getTime();
+            db.collection('thermometers').doc(teamName).set(objNew).then(() => {
+                    console.log(`Created new thermometer for ${teamName}`);
+                    document.location.href = 'taskbar.html';
+                })
+                .catch((error) => {
+                    console.error('Error adding document: ', error);
+                });
+        })
         .catch((error) => {
-          console.error('Error adding document: ', error);
+            console.error('Error adding document: ', error);
         });
-    })
-    .catch((error) => {
-      console.error('Error adding document: ', error);
-    });
 });
 
 const cancelButton = document.getElementById('cancelBtn');
