3d35f8f9de0ad77074ce24acf6755dc79dac0ea0
kyoko1011 <kmyokota@ucsd.edu>
2020-06-10 20:33:02 -0700

    Reformatted integration tests to be split into two seperate sections

--- test/integration_testing/spec.js
+++ test/integration_testing/spec.js
@@ -7,7 +7,7 @@ const loadTime = 1500;
 const invalidName = 'invalidNameShouldNotWork';
 const sleep = time => new Promise(r => setTimeout(r, time));
 
-describe('Integration Testing will now start!', async function(){
+describe('Integration Testing will now start for Create Team, Join Team, Leave Team, Log Out!', async function(){
   this.timeout(1000000)
 
   before(async function() {
@@ -24,91 +24,96 @@ describe('Integration Testing will now start!', async function(){
       throw new Error("Spectron random error, please try again");
     }
   })
-  beforeEach(async function() {
-    try {
-      await this.app.client.click('#leaveTeamButton');
-    } catch (e) {}
-  })
   after(async function () {
     await sleep(loadTime);
     await this.app.stop();
   })
 
-  describe('Create Team Functionality', async function() {
-    it('tests createTeam cancel', async function () {
-      await sleep(loadTime);
-      await this.app.client.click('#createTeamButton');
-      await sleep(loadTime);
-      await this.app.client.setValue('#teamName', 'testTeam');
-      await this.app.client.click('#cancelBtn');
-      await sleep(loadTime);
-    })
-    it('tests createTeam with pre-existing team name', async function () {
-      await sleep(loadTime);
-      await this.app.client.click('#createTeamButton');
-      await sleep(loadTime);
-      await this.app.client.setValue('#teamName', 'testTeam');
-      await this.app.client.click('#createBtn');
-      await sleep(loadTime);
-    })
-  });
-
-  describe('Join Team Functionality', async function(){
-    it('tests joinTeam cancel', async function () {
-      await sleep(loadTime);
-      await this.app.client.click('#joinTeamButton');
-      await sleep(loadTime);
-      await this.app.client.setValue('#teamName', 'testTeam');
-      await this.app.client.click('#cancelBtn');
-      await sleep(loadTime);
-    })
-
-    it('tests joinTeam join invalid name', async function () {
-      await sleep(loadTime);
-      await this.app.client.click('#joinTeamButton');
-      await sleep(loadTime);
-      await this.app.client.setValue('#teamName', invalidName);
-      await this.app.client.click('#joinBtn');
-      await sleep(loadTime);
+  describe('Exterior functionality', async function() {
+    beforeEach(async function() {
       try {
-        //See if back on it went through and now on taskbar taskbar
-        await this.app.client.click('leaveTeamButton');
+        await this.app.client.click('#leaveTeamButton');
+      } catch (e) {}
+    })
+    describe('Create Team Functionality', async function () {
+      it('tests createTeam cancel', async function () {
         await sleep(loadTime);
-        assert(false, 'Test is allowed to join invalid name: ' + invalidName);
-      } catch (e) {
-        //If caught error then it works properly
+        await this.app.client.click('#createTeamButton');
         await sleep(loadTime);
-        assert(true, 'Test is not allowed to join invalid name: ' + invalidName);
-        //Get back to taskbar for next test
+        await this.app.client.setValue('#teamName', 'testTeam');
         await this.app.client.click('#cancelBtn');
         await sleep(loadTime);
-      }
-    })
-
-    it('tests joinTeam join valid name', async function () {
-      await sleep(loadTime);
-      await this.app.client.click('#joinTeamButton');
-      await sleep(loadTime);
-      await this.app.client.setValue('#teamName', 'testTeam');
-      await this.app.client.click('#joinBtn');
-      await sleep(loadTime);
-    })
-  });
-
-  describe('Log Out Functionality', async function () {
-
-    it('test if logOut button actually logs out user', async function() {
-      await sleep(loadTime);
-      await this.app.client.click('#logOutBtn');
-      await sleep(loadTime);
-      //Make sure logged out successfully
-      try {
-        //Should be on sign-in page, try to sign in to catch
-        await this.app.client.click('#signInBtn');
+      })
+      it('tests createTeam with pre-existing team name', async function () {
         await sleep(loadTime);
-      } catch (e) {
-        assert(false, 'Test does not log-out on log-out click');
-      }
-    })
+        await this.app.client.click('#createTeamButton');
+        await sleep(loadTime);
+        await this.app.client.setValue('#teamName', 'testTeam');
+        await this.app.client.click('#createBtn');
+        await sleep(loadTime);
+      })
+    });
+
+    describe('Join Team Functionality', async function () {
+      it('tests joinTeam cancel', async function () {
+        await sleep(loadTime);
+        await this.app.client.click('#joinTeamButton');
+        await sleep(loadTime);
+        await this.app.client.setValue('#teamName', 'testTeam');
+        await this.app.client.click('#cancelBtn');
+        await sleep(loadTime);
+      })
+
+      it('tests joinTeam join invalid name', async function () {
+        await sleep(loadTime);
+        await this.app.client.click('#joinTeamButton');
+        await sleep(loadTime);
+        await this.app.client.setValue('#teamName', invalidName);
+        await this.app.client.click('#joinBtn');
+        await sleep(loadTime);
+        try {
+          //See if back on it went through and now on taskbar taskbar
+          await this.app.client.click('leaveTeamButton');
+          await sleep(loadTime);
+          assert(false, 'Test is allowed to join invalid name: ' + invalidName);
+        } catch (e) {
+          //If caught error then it works properly
+          await sleep(loadTime);
+          assert(true, 'Test is not allowed to join invalid name: ' + invalidName);
+          //Get back to taskbar for next test
+          await this.app.client.click('#cancelBtn');
+          await sleep(loadTime);
+        }
+      })
+
+      it('tests joinTeam join valid name', async function () {
+        await sleep(loadTime);
+        await this.app.client.click('#joinTeamButton');
+        await sleep(loadTime);
+        await this.app.client.setValue('#teamName', 'testTeam');
+        await this.app.client.click('#joinBtn');
+        await sleep(loadTime);
+      })
+    });
+
+    describe('Log Out Functionality', async function () {
+
+      it('test if logOut button actually logs out user', async function () {
+        await sleep(loadTime);
+        await this.app.client.click('#logOutBtn');
+        await sleep(loadTime);
+        //Make sure logged out successfully
+        try {
+          //Should be on sign-in page, try to sign in to catch
+          await this.app.client.click('#signInBtn');
+          await sleep(loadTime);
+        } catch (e) {
+          assert(false, 'Test does not log-out on log-out click');
+        }
+      })
+    });
   });
+  describe('Interior Functionality', async function() {
+    /** INSERT CHECKIN/OUT/STATUS FUNCTIONS HERE **/
+  })
 });
